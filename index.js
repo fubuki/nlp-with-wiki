5758538d37ebf8f97017c53256cc4bd20a64960d
fubuki <fubuki@users.noreply.github.com>
2014-03-28 22:41:44 +0800

    建立mongodb連線

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var natural = require('natural'),
+
     tokenizer =new natural.WordTokenizer();
 console.log(tokenizer.tokenize("your dog has flees."));
+
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1/scrapy');
+
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+  
+var html = new Schema({
+    title     : String
+  , body      : String
+
+});
