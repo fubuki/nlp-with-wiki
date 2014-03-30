d444ea6c161305037197b10cc59adce9b90ef7c3
fubuki <fubuki@users.noreply.github.com>
2014-03-30 13:09:58 +0800

    測試從mongodb抓出新聞標題並進行n-gram處理。

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
 var natural = require('natural'),
     tokenizer = new natural.WordTokenizer();
 
-console.log(tokenizer.tokenize("your dog has flees."));
-
 var mongoose = require('mongoose');
-
+var NGrams = natural.NGrams;
 mongoose.connect('mongodb://127.0.0.1/scrapy');
 
-var Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId;
+var Schema = mongoose.Schema;
   
 var html = new Schema({
-    title     : String
-  , body      : String
+    title     : { type: String }
 });
 
-var Model = mongoose.model('yahoo', html);
+mongoose.model('yahoo', html);
+var yahoo = mongoose.model('yahoo')
 
-Model.find({}, function (err, docs) {
-	console.log(docs);
+
+yahoo.findOne(function (err, docs) {
+	//console.log(docs.title);
+	processData(docs.title);
 });
 
 
-tokenizer = new natural.RegexpTokenizer({pattern: /\-/});
-console.log(tokenizer.tokenize("flee-dog"));
-// [ 'flee', 'dog' ]
-
-console.log(natural.JaroWinklerDistance("dixon","dicksonx"))
-console.log(natural.JaroWinklerDistance('not', 'same'));
-
-natural.PorterStemmer.attach();
-console.log("i am waking up to the sounds of chainsaws".tokenizeAndStem());
-console.log("chainsaws".stem());
-
-
-classifier = new natural.BayesClassifier();
-
-classifier.addDocument('i am long qqqq', 'buy');
-classifier.addDocument('buy the q s', 'buy');
-classifier.addDocument('short gold', 'sell');
-classifier.addDocument('sell gold', 'sell');
-
-classifier.train();
-
-
-var NGrams = natural.NGrams;
-
-console.log(NGrams.bigrams('some words here'));
-console.log(NGrams.bigrams(['some', 'words', 'here']));
-
-console.log(NGrams.trigrams('some other words here'));
-console.log(NGrams.trigrams(['some', 'other', 'words', 'here']));
-
-console.log(NGrams.ngrams('some other words here for you', 4));
-console.log(NGrams.ngrams(['some', 'other', 'words', 'here', 'for', 'you'], 4));
+function processData(doc)
+{
+	console.log(NGrams.ngrams(doc, 4));	
+}
