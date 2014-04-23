21dcf4bbb40d720105161bb9309a9865c6a46e36
fubuki <fubuki@users.noreply.github.com>
2014-04-23 22:37:13 +0800

    新增一個Latent Dirichlet Allocation的範例。

---
+++ LDA.py
@@ -0,0 +1,11 @@
+import logging, gensim, bz2
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+id2word = gensim.corpora.Dictionary.load_from_text('/root/wiki_en_wordids.txt')
+
+mm = gensim.corpora.MmCorpus('wiki_en_tfidf.mm')
+
+lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=10000, passes=1)
+
+lda.print_topics(20)
